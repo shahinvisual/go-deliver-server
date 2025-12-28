4ece14834433a3039df7f0b58de9e28f941405c6
shahinvisual <shahinalam1084@gmail.com>
2025-12-28 12:41:30 +0600

    stripe payment intrigration and successfully bill paid and data store in database

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const cors = require("cors");
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require("dotenv").config();
+const stripe = require('stripe')(process.env.Payment_secret);
 const PORT = process.env.PORT || 5000;
 
 
@@ -49,6 +50,20 @@ async function run() {
                 res.status(500).send({ message: 'Failed to get parcels' })
             }
         });
+        // ==================Parcel Data get by Id==============================================
+        app.get('/parcels/:id', async (req, res) => {
+            try {
+                const id = req.params.id;
+                const parcels = await parcelCollection.findOne({ _id: new ObjectId(id) });
+                if (!parcels) {
+                    return res.status(404).send({ message: 'parcels not found' });
+                };
+                res.send(parcels)
+            } catch (error) {
+                console.error('Error parcel fetching', error);
+                res.status(500).send({ message: 'Failed to fetch parcels' })
+            }
+        })
 
 
         // ==================Parcel Data Save in DB==============================================
@@ -75,6 +90,60 @@ async function run() {
             }
         });
 
+        // =================Create Payment Intent=========================
+        app.post('/create-payment-intent', async (req, res) => {
+            try {
+                const amountInCents = req.body.amountCents;
+                const paymentIntent = await stripe.paymentIntents.create({
+                    amount: amountInCents,
+                    currency: 'usd',
+                    payment_method_types: ['card']
+                });
+                res.json({ clientSecret: paymentIntent.client_secret })
+            } catch {
+                res.status(500).json({ error: error.message })
+            }
+        });
+
+        // ==========Post: Record Payment and update Parcels status=====================
+        app.post('/payments', async (req, res) => {
+            try {
+                const { id, email, amount, paymentMethod, transactionId } = req.body;
+
+                // 1: update parcel's payment status
+                const updateResult = await parcelCollection.updateOne(
+                    { _id: new ObjectId(id) },
+                    {
+                        $set: {
+                            payment_status: 'paid'
+                        }
+                    }
+                );
+
+                if (updateResult.modifiedCount === 0) {
+                    return res.status(404).send({ message: 'parcels not found or already paid' });
+                };
+
+                // Insert Payment Record==============
+                const paymentDoc = {
+                    id,
+                    email,
+                    amount,
+                    paymentMethod,
+                    transactionId,
+                    paid_at_string: new Date().toISOString(),
+                    paid_at: new Date()
+                };
+                const paymentResult = await paymentsCollection.insertOne(paymentDoc);
+                res.status(201).send({
+                    message: 'Payment Recorded and Parcel marked as paid',
+                    insertedId: paymentResult.insertedId;
+                });
+            } catch (error) {
+                console.error('payment processing error:', error);
+                res.status(500).send({ message: 'Failed to record paymentF' })
+            }
+        })
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
